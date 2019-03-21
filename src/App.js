af8e2cc9d78c9de8362be59813d712bebef0a70d
AJLambda <apj2277@gmail.com>
2019-03-21 00:17:08 -0700

    finished MVP Day 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,94 @@
 import React from 'react';
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+
+//Array of Todo objects
+
+// The task field is the todo title that will be shown to the user.
+// The completed field should default to false and will be the field that we toggle when we complete a todo.
+// The id field is a unique Time Stamp that will be assigned by Date.now().
+
+const todoData = [
+  {
+    task: 'Feed the dog',
+    id: 0,
+    completed: false
+  },
+  {
+    task: 'Walk the human',
+    id: 1,
+    completed: false
+  },
+  {
+    task: 'Take out the clothes',
+    id: 2,
+    completed: false
+  },
+  {
+    task: 'Wash the trash',
+    id: 3,
+    completed: false
+  },
+]
+
+// you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todoList: todoData,
+      task: ''
+    };
+  };
+  
+  handleChanges = event => {
+    // update the todo property on state
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  addTodo = event => {
+    event.preventDefault();
+    this.setState({
+      todoList: [...this.state.todoList, 
+        {
+          task: this.state.task,
+          id: Date.now(), 
+          completed: false
+        }
+      ],
+      task: ''
+    });
+  };
+  
+
+  
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+
+        <h2>Todo List: MVP</h2>
+
+        <TodoList 
+
+          todoList={this.state.todoList} 
+
+        />
+
+        <TodoForm 
+
+          handleChanges={this.handleChanges}
+          addTodo={this.addTodo}
+          task={this.state.task}
+
+        />
+        
       </div>
     );
   }
